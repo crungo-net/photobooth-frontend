be4377580abf563340cc9668b6ffdd2055c74be2
Vincent Chu <vincentchu3@gmail.com>
2026-03-14 19:21:34 -0700

    custom styling for delete button

--- src/components/mediaviewer/PageToolbar.vue
+++ src/components/mediaviewer/PageToolbar.vue
@@ -2,50 +2,24 @@
   <q-page-sticky position="bottom" id="gallery-toolbar" class="toolbar" v-if="item">
     <div class="q-mb-lg action-buttons col">
       <div class="q-pa-md row flex flex-center">
-        <q-btn
-          v-if="showDelete"
-          stack
-          rounded
-          class="q-mr-sm action-button action-button-delete col-auto glass-effect"
-          color="primary"
-          no-caps
-          icon="sym_o_delete"
-          :label="$t('BTN_LABEL_GALLERY_DELETE')"
-          @click="confirmDeleteDialog = true"
-        />
+        <q-btn v-if="showDelete" stack flat no-ripple rounded
+          class="q-mr-sm action-button action-button-delete col-auto" no-caps icon="img:/api/icons/delete.png"
+          @click="confirmDeleteDialog = true" />
 
-        <q-btn
-          v-if="showFilter && enableFilter"
-          stack
-          no-caps
-          rounded
-          color="primary"
-          class="q-mr-sm action-button action-button-filter col-auto glass-effect"
-          icon="sym_o_filter"
-          :label="$t('BTN_LABEL_GALLERY_FILTER')"
-          @click="invokeToggleDisplayFilter"
-        />
-        <q-btn
-          v-if="showDownload"
-          stack
-          no-caps
-          rounded
-          color="primary"
-          class="q-mr-sm action-button action-button-download col-auto glass-effect"
-          icon="sym_o_download"
-          :label="$t('BTN_LABEL_GALLERY_DOWNLOAD')"
-          @click="openURL(`/sharepage/#?url=/media/full/${item.id}`)"
-        />
-        <ShareTriggerButtons
-          v-if="showShare"
-          :triggers="shareButtons"
-          :current-item-is-image="isPrintableImage(item.unprocessed)"
-          @trigger-action="invokeShareAction"
-        ></ShareTriggerButtons>
+        <q-btn v-if="showFilter && enableFilter" stack no-caps rounded color="primary"
+          class="q-mr-sm action-button action-button-filter col-auto glass-effect" icon="sym_o_filter"
+          :label="$t('BTN_LABEL_GALLERY_FILTER')" @click="invokeToggleDisplayFilter" />
+        <q-btn v-if="showDownload" stack no-caps rounded color="primary"
+          class="q-mr-sm action-button action-button-download col-auto glass-effect" icon="sym_o_download"
+          :label="$t('BTN_LABEL_GALLERY_DOWNLOAD')" @click="openURL(`/sharepage/#?url=/media/full/${item.id}`)" />
+        <ShareTriggerButtons v-if="showShare" :triggers="shareButtons"
+          :current-item-is-image="isPrintableImage(item.unprocessed)" @trigger-action="invokeShareAction">
+        </ShareTriggerButtons>
       </div>
 
       <div class="q-mr-sm row flex flex-center">
-        <q-badge color="grey-8" class="q-mr-xs"> <q-icon name="sym_o_image" color="white" class="q-mr-xs" /> {{ item.id }}</q-badge>
+        <q-badge color="grey-8" class="q-mr-xs"> <q-icon name="sym_o_image" color="white" class="q-mr-xs" /> {{ item.id
+          }}</q-badge>
         <q-badge color="grey-8" class="q-mr-xs" v-if="props.image_number && props.images_total">
           <q-icon name="sym_o_tag" color="white" class="q-mr-xs" />
           {{
@@ -67,7 +41,8 @@
 
       <q-card-actions align="right">
         <q-btn v-close-popup flat :label="$t('BTN_LABEL_CANCEL')" />
-        <q-btn v-close-popup :label="$t('BTN_LABEL_DELETE_IMAGE')" color="primary" @click="[invokeDeleteMediaitem(item.id), $emit('closeEvent')]" />
+        <q-btn v-close-popup :label="$t('BTN_LABEL_DELETE_IMAGE')" color="primary"
+          @click="[invokeDeleteMediaitem(item.id), $emit('closeEvent')]" />
       </q-card-actions>
     </q-card>
   </q-dialog>
